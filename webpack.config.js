2286050b161523385b756785a2788836917a4da4
Ciro S. Costa <ciro.costa@usp.br>
2014-10-21 01:09:43 -0200

    f commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+  entry: './src/main.jsx',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'main.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader!sass-loader?includePaths[]=' +
+                 __dirname + '/src'
+      },
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'jsx-loader?harmony'
+      }
+    ]
+  }
+};
