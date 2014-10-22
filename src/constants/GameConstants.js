05863b187ef1cb2eedbe1f8a31d9579c25041157
Ciro S. Costa <ciro.costa@usp.br>
2014-10-22 14:51:38 -0200

    updates styling

--- src/constants/GameConstants.js
+++ src/constants/GameConstants.js
@@ -2,7 +2,7 @@ var {keyMirror} = require('../utils');
 
 module.exports = keyMirror({
   STATES: ['tile-invalid', 'tile-valid', 'tile-passed', 'tile-start'],
-  SQUARE_SIZE: [50, 101],
+  SQUARE_SIZE: [60, 120],
 
   NEXT_STATE: 'GAME_NEXT_STATE',
   CLICK: 'GAME_CLICK',
